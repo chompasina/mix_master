9eb91ced88f3d6187c95cc0d0e8b5f71339c255a
Tommasina <tommasina1@gmail.com>
2016-07-17 12:11:16 -0600

    added song functionality to features

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ group :development, :test do
   gem 'capybara'
   gem 'launchy'
   gem 'shoulda-matchers', github: 'thoughtbot/shoulda-matchers'
+  gem 'database_cleaner'
+  gem 'factory_girl_rails'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :artists, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :artists do
+    resources :songs, only: [:new, :create]
+  end
+
+    resources :songs, only: [:show]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,4 +60,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+# end
