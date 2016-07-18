4f8a41ecff8f6a1b6b7c818dee12b91a19628716
Tommasina <tommasina1@gmail.com>
2016-07-18 09:19:12 -0600

    added root path to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'artists#index'
+  
   resources :artists, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :artists do
     resources :songs, only: [:new, :create]
